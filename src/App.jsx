8573553467d07ae9cd045c4ae6b9ae80db4c64ad
Vlad Metelev <36455557+VlMet@users.noreply.github.com>
2025-04-25 01:52:47 +0500

    12

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,19 @@
-import { AppBar, Toolbar, Typography, Drawer, List, ListItem, ListItemIcon, ListItemText, Box, Container } from '@mui/material';
-import InboxIcon from '@mui/icons-material/Inbox';
+import { AppBar, Toolbar, Typography, Box, Card, Grid, Container } from '@mui/material';
+import AppHeader from './components/AppHeader';
 
 export default function App() {
   return (
-    <Box sx={{ display: 'flex' }}>
-      {/* Шапка */}
-      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-        <Toolbar>
-          <Typography variant="h6">My App</Typography>
-        </Toolbar>
-      </AppBar>
-
-      {/* Боковое меню */}
-      <Drawer
-        variant="permanent"
-        sx={{
-          width: 240,
-          flexShrink: 0,
-          [`& .MuiDrawer-paper`]: { width: 240, boxSizing: 'border-box' },
-        }}
-      >
-        <Toolbar /> {/* Отступ под AppBar */}
-        <List>
-          {['Item 1', 'Item 2'].map((text) => (
-            <ListItem button key={text}>
-              <ListItemIcon><InboxIcon /></ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-
-      {/* Основной контент */}
-      <Box component="main" sx={{ flexGrow: 1, p: 3, marginLeft: '240px', marginTop: '64px' }}>
-        <Container>
-          <Typography paragraph>Основной контент приложения...</Typography>
-        </Container>
-      </Box>
-
-      {/* Подвал */}
-      <Box component="footer" sx={{ py: 3, textAlign: 'center' }}>
-        <Typography>Footer © 2024</Typography>
-      </Box>
+    <Box sx={{ height: "100vh" }}>
+      <AppHeader />
+      
+      <Grid container sx={{ height: "calc(100vh - 64px)", p: 2 }} spacing={2}>
+        <Grid item size={{xs:12, sm: 5, lg:3}} sx={{ height: "100%" }}>
+          <Card sx={{ height: "100%", p: 2 }}>123</Card>
+        </Grid>
+        <Grid item size={{xs:12, sm: 7, lg:9}} sx={{ height: "100%" }}>
+          <Card sx={{ height: "100%", p: 2 }}>456</Card>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,11 @@
+import { AppBar, Toolbar, Typography } from '@mui/material'
+
+export default function AppHeader() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6">App</Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
